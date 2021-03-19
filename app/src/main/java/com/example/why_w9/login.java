6d5ccf9e5ae1d81875680bdb12ba45e4cac72e46
David <drj63@scarletmail.rutgers.edu>
2021-03-19 18:59:11 -0400

    register

--- app/src/main/java/com/example/why_w9/login.java
+++ app/src/main/java/com/example/why_w9/login.java
@@ -1,13 +1,15 @@
 package com.example.why_w9;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class login extends Activity
-{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
     }
+
 }

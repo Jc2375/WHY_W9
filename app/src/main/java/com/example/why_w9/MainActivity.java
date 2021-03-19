57fb024c6907711bff884f99a54260f6c0ce60c0
Jason Chan <jc2375@scarletmail.rutgers.edu>
2021-03-19 18:53:54 -0400

    test

--- app/src/main/java/com/example/why_w9/MainActivity.java
+++ app/src/main/java/com/example/why_w9/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.why_w9;
 //this project causes pain
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onButtonClick(View v)
+    {
+        if(v.getId()== R.id.Blogin)
+        {
+            Intent i=new Intent(MainActivity.this, login.class );
+            startActivity(i);
+        }
+    }
 }
+
---
+++ app/src/main/java/com/example/why_w9/login.java
@@ -0,0 +1,13 @@
+package com.example.why_w9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class login extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}

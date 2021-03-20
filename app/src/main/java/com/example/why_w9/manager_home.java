8712a535f8d11287950b20f9def3a595df518678
David <drj63@scarletmail.rutgers.edu>
2021-03-20 18:52:39 -0400

    employee update

--- app/src/main/java/com/example/why_w9/manager_home.java
+++ app/src/main/java/com/example/why_w9/manager_home.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class manager_home extends Activity
 {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manager_home);
+
     }
 }

9526c7e61d21a63e8de13050ce3600be7560a06f
Jason Chan <jc2375@scarletmail.rutgers.edu>
2021-03-21 19:13:40 -0400

    host, and chef main page

---
+++ app/src/main/java/com/example/why_w9/payment_type.java
@@ -0,0 +1,17 @@
+package com.example.why_w9;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class payment_type extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_type);
+
+    }
+}
+

e645a59da249ae43230fcfb935fecc19c2374afe
Jason Chan <jc2375@scarletmail.rutgers.edu>
2021-03-21 19:10:48 -0400

    host, and chef main page

--- app/src/main/java/com/example/why_w9/menu_home_canorder.java
+++ app/src/main/java/com/example/why_w9/menu_home_canorder.java
@@ -1,6 +1,9 @@
 package com.example.why_w9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,13 @@ public class menu_home_canorder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_home_canorder);
+    }
+    public void onButtonClick(View v) {
+        if (v.getId() == R.id.BCashPayment) {
+            Toast.makeText(menu_home_canorder.this, "Select Choice of Payment", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(menu_home_canorder.this, payment_type.class);
+            startActivity(i);
+        }
+
     }
 }

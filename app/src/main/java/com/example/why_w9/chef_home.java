53c52f25248e32e7c89e3953fc3aea8ff88632d0
Jason Chan <jc2375@scarletmail.rutgers.edu>
2021-03-21 16:26:23 -0400

    host, and chef main page

---
+++ app/src/main/java/com/example/why_w9/chef_home.java
@@ -0,0 +1,14 @@
+package com.example.why_w9;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class chef_home extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chef_home);
+    }
+
+}

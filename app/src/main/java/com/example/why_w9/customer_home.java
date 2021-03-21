73720d387207723f15aaf9af58cacf812ba51f82
David <drj63@scarletmail.rutgers.edu>
2021-03-21 14:03:55 -0400

    -Logout - Customer/Manager -Fixed Login/Register To Not Look Odd Anymore

--- app/src/main/java/com/example/why_w9/customer_home.java
+++ app/src/main/java/com/example/why_w9/customer_home.java
@@ -4,17 +4,48 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth; //logout part 1
+import com.google.firebase.auth.FirebaseUser; //logout part 2
+
 public class customer_home extends AppCompatActivity
 {
+    private FirebaseAuth mFirebaseAuth; //logout part 3
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_home);
+
+        mFirebaseAuth = FirebaseAuth.getInstance(); //logout part 4
     }
+
+    //Logout part 5
+    protected void onStart()
+    {
+        super.onStart();
+        FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null){
+            startActivity(new Intent (this,login.class));
+        }
+
+    }
+    public void onButtonClick(View v) {
+        if (v.getId() == R.id.bSignOut) {
+            mFirebaseAuth.signOut();
+            Toast.makeText(customer_home.this, "User Signed Out Successfully", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(customer_home.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+    //Up to here
+
 }
+
+
--- app/src/main/java/com/example/why_w9/manager_home.java
+++ app/src/main/java/com/example/why_w9/manager_home.java
@@ -1,17 +1,29 @@
 package com.example.why_w9;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class manager_home extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manager_home);
 
     }
+
+    public void onButtonClick(View v) {
+        if (v.getId() == R.id.bSignOut) {
+            Toast.makeText(manager_home.this, "User Signed Out Successfully", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(manager_home.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+
 }

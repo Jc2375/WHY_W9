670bc1ab28c086ba5d3e6f423348a84a676afc1a
Jason Chan <jc2375@scarletmail.rutgers.edu>
2021-03-22 19:07:36 -0400

    order type and payment type updates, as well as button name updates

---
+++ app/src/main/java/com/example/why_w9/busboy_home.java
@@ -0,0 +1,25 @@
+package com.example.why_w9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class busboy_home extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.busboy_home);
+    }
+    public void onButtonClick(View v) {
+
+        if (v.getId() == R.id.BBusboySignout) {
+            Intent i = new Intent(busboy_home.this, MainActivity.class);
+            startActivity(i);
+        }
+
+    }
+}

e832670dd4ed47379a8cf21ca145901f2e1db390
juergen <juergenbenitez@gmail.com>
2021-03-23 20:15:33 -0400

    Got chef schedules to update using a spinner instead of typing manually.

--- app/src/main/java/com/example/why_w9/ChefSchedules.java
+++ app/src/main/java/com/example/why_w9/ChefSchedules.java
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ExpandableListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class ChefSchedules extends AppCompatActivity {
+public class ChefSchedules extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     ExpandableListViewAdapter listViewAdapter;
     ExpandableListView expandableListView;
     List<String> chefList;
@@ -28,6 +30,25 @@ public class ChefSchedules extends AppCompatActivity {
         makeChefSchedules();
         listViewAdapter = new ExpandableListViewAdapter(this, chefList, chefInfo);
         expandableListView.setAdapter(listViewAdapter);
+
+        Spinner daySpinner = findViewById(R.id.dayOptionSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.dayOption, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        daySpinner.setAdapter(adapter);
+        daySpinner.setOnItemSelectedListener(this);
+
+        Spinner timesSpinner = findViewById(R.id.timeSpinnerStart);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.timeOptions, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timesSpinner.setAdapter(adapter2);
+        timesSpinner.setOnItemSelectedListener(this);
+
+        Spinner timesSpinner2 = findViewById(R.id.timeSpinnerEnd);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.timeOptions, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timesSpinner2.setAdapter(adapter3);
+        timesSpinner2.setOnItemSelectedListener(this);
+
 
     }
     public void makeChefSchedules(){
@@ -57,17 +78,20 @@ public class ChefSchedules extends AppCompatActivity {
     }
 
     public void addShift(View v) {
-        EditText startTime_tv = (EditText) findViewById(R.id.editTextStartTime);
-        String start = startTime_tv.getText().toString();
+        //get start time from spinner
+        Spinner startTime = (Spinner)findViewById(R.id.timeSpinnerStart);
+        String start = startTime.getSelectedItem().toString();
 
-        EditText endTime_tv = (EditText) findViewById(R.id.editTextEndTime);
-        String end = endTime_tv.getText().toString();
+        //get end time from spinner
+        Spinner endTime = (Spinner)findViewById(R.id.timeSpinnerEnd);
+        String end = endTime.getSelectedItem().toString();
 
         EditText chefName_tv = (EditText) findViewById(R.id.editTextChefName);
         String chefName = chefName_tv.getText().toString();
 
-        EditText dayField_tv = (EditText) findViewById(R.id.editTextDay);
-        String day = dayField_tv.getText().toString();
+        //get day option from spinner
+        Spinner day_spin = (Spinner)findViewById(R.id.dayOptionSpinner);
+        String dayField = day_spin.getSelectedItem().toString();
 
         if (chefName.compareTo(chefList.get(0)) == 0) {
 
@@ -81,25 +105,25 @@ public class ChefSchedules extends AppCompatActivity {
             newDays.add((String) listViewAdapter.getChild(0,6));
 
 
-            if (day.compareTo("Monday") == 0) {
+            if (dayField.compareTo("Monday") == 0) {
                 newDays.set(0,"Monday:" + start + "-" + end);
             }
-            if (day.compareTo("Tuesday") == 0) {
+            if (dayField.compareTo("Tuesday") == 0) {
                 newDays.set(1,"Tuesday:" + start + "-" + end);
             }
-            if (day.compareTo("Wednesday") == 0) {
+            if (dayField.compareTo("Wednesday") == 0) {
                 newDays.set(2,"Wednesday:" + start + "-" + end);
             }
-            if (day.compareTo("Thursday") == 0) {
+            if (dayField.compareTo("Thursday") == 0) {
                 newDays.set(3,"Thursday:" + start + "-" + end);
             }
-            if (day.compareTo("Friday") == 0) {
+            if (dayField.compareTo("Friday") == 0) {
                 newDays.set(4,"Friday:" + start + "-" + end);
             }
-            if (day.compareTo("Saturday") == 0) {
+            if (dayField.compareTo("Saturday") == 0) {
                 newDays.set(5,"Saturday:" + start + "-" + end);
             }
-            if (day.compareTo("Sunday") == 0) {
+            if (dayField.compareTo("Sunday") == 0) {
                 newDays.set(6,"Sunday:" + start + "-" + end);
             }
             listViewAdapter.updateExpListView(chefList,newDays,0);
@@ -114,28 +138,38 @@ public class ChefSchedules extends AppCompatActivity {
             newDays.add((String) listViewAdapter.getChild(1,5));
             newDays.add((String) listViewAdapter.getChild(1,6));
 
-            if (day.compareTo("Monday") == 0) {
+            if (dayField.compareTo("Monday") == 0) {
                 newDays.set(0,"Monday:" + start + "-" + end);
             }
-            if (day.compareTo("Tuesday") == 0) {
+            if (dayField.compareTo("Tuesday") == 0) {
                 newDays.set(1,"Tuesday:" + start + "-" + end);
             }
-            if (day.compareTo("Wednesday") == 0) {
+            if (dayField.compareTo("Wednesday") == 0) {
                 newDays.set(2,"Wednesday:" + start + "-" + end);
             }
-            if (day.compareTo("Thursday") == 0) {
+            if (dayField.compareTo("Thursday") == 0) {
                 newDays.set(3,"Thursday:" + start + "-" + end);
             }
-            if (day.compareTo("Friday") == 0) {
+            if (dayField.compareTo("Friday") == 0) {
                 newDays.set(4, "Friday:" + start + "-" + end);
             }
-            if (day.compareTo("Saturday") == 0) {
+            if (dayField.compareTo("Saturday") == 0) {
                 newDays.set(5,"Saturday:" + start + "-" + end);
             }
-            if (day.compareTo("Sunday") == 0) {
+            if (dayField.compareTo("Sunday") == 0) {
                 newDays.set(6,"Sunday:" + start + "-" + end);
             }
             listViewAdapter.updateExpListView(chefList,newDays,1);
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }

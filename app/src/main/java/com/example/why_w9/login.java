885e53a9933dbc5806f50409edffc953191079e2
rlv31 <65618861+rlv31@users.noreply.github.com>
2021-04-09 16:56:32 -0400

    Added to employee registration button

--- app/src/main/java/com/example/why_w9/login.java
+++ app/src/main/java/com/example/why_w9/login.java
@@ -15,6 +15,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class login extends AppCompatActivity {
 
@@ -33,87 +35,96 @@ public class login extends AppCompatActivity {
         fAuth = FirebaseAuth.getInstance();
     }
 
-        public void onButtonClick(View v)
+    public void onButtonClick(View v)
+    {
+        switch (v.getId())
         {
-            switch (v.getId())
+            case R.id.BLoginAttempt:
             {
-                case R.id.BLoginAttempt:
-                    {
-                        String email = mEmail.getText().toString().trim();
-                        String password = mPassword.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
-                        if(TextUtils.isEmpty(email))
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    mPassword.setError("Password is Required");
+                    return;
+                }
+                if(password.length() < 6)
+                {
+                    mPassword.setError("Password must be greater than 5 characters!");
+                    return;
+                }
+                if(email.equals("manager@gmail.com")&&password.equals("managerpassword"))
+                {
+                    Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), manager_home.class));
+                    return;
+                }
+                if(email.equals("driver@gmail.com")&&password.equals("driverpassword"))
+                {
+                    Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), driver_home.class));
+                    return;
+                }
+                if(email.equals("waiter@gmail.com")&&password.equals("waiterpassword"))
+                {
+                    Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), waiter_home.class));
+                    return;
+                }
+                if(email.equals("chef@gmail.com")&&password.equals("chefpassword"))
+                {
+                    Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), chef_home.class));
+                    return;
+                }
+                if(email.equals("host@gmail.com")&&password.equals("hostpassword"))
+                {
+                    Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), host_home.class));
+                    return;
+                }
+                if(email.equals("busboy@gmail.com")&&password.equals("busboypassword"))
+                {
+                    Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), busboy_home.class));
+                    return;
+                }
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
                         {
-                            mEmail.setError("Email is Required");
-                            return;
-                        }
-                        if(TextUtils.isEmpty(password))
-                        {
-                            mPassword.setError("Password is Required");
-                            return;
-                        }
-                        if(password.length() < 6)
-                        {
-                            mPassword.setError("Password must be greater than 5 characters!");
-                            return;
-                        }
-                        if(email.equals("manager@gmail.com")&&password.equals("managerpassword"))
-                        {
+
+
                             Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), manager_home.class));
-                            return;
-                        }
-                        if(email.equals("driver@gmail.com")&&password.equals("driverpassword"))
-                        {
-                            Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), driver_home.class));
-                            return;
-                        }
-                        if(email.equals("waiter@gmail.com")&&password.equals("waiterpassword"))
-                        {
-                            Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), waiter_home.class));
-                            return;
-                        }
-                        if(email.equals("chef@gmail.com")&&password.equals("chefpassword"))
-                        {
-                            Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), chef_home.class));
-                            return;
-                        }
-                        if(email.equals("host@gmail.com")&&password.equals("hostpassword"))
-                        {
-                            Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), host_home.class));
-                            return;
-                        }
-                        if(email.equals("busboy@gmail.com")&&password.equals("busboypassword"))
-                        {
-                            Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), busboy_home.class));
-                            return;
-                        }
-                        fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful())
-                                {
-                                    Toast.makeText(login.this, "User Signed In", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(getApplicationContext(), customer_home.class));
-                                }
-                                else {
-                                    Toast.makeText(login.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                }
+
+                            String uid= FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            DatabaseReference reff;
+                            reff = FirebaseDatabase.getInstance().getReference().child("User").child(uid).child("User_type");
+
+                            if(reff.equals("Customer")) {
+
+                                startActivity(new Intent(getApplicationContext(), customer_home.class));
                             }
-                        });
+                        }
+                        else {
+                            Toast.makeText(login.this, "Error" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
                        /* FirebaseDatabase database=FirebaseDatabase.getInstance();
                         DatabaseReference table_user =database.getReference("User");*/
 
-                    }
             }
-
-
         }
+
 
     }
 
+}

29082f2302b425b447a1381edba6f463bc50d857
David <drj63@scarletmail.rutgers.edu>
2021-04-20 21:40:20 -0400

    reverted hardcore in table.java

--- app/src/main/java/com/example/why_w9/table.java
+++ app/src/main/java/com/example/why_w9/table.java
@@ -26,24 +26,337 @@ public class table extends AppCompatActivity
         setContentView(R.layout.table);
 
 
-        DatabaseReference reff;
-        reff = FirebaseDatabase.getInstance().getReference("Table").child(String.valueOf(global.Global.TN));
+        if(global.Global.TN == 1)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("1");
 
-        reff.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String Status = dataSnapshot.child("Status").getValue().toString();
-                String TableNumber = reff.toString();
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
 
-                ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
-                ((TextView) findViewById(R.id.tableNumber)).setText(TableNumber);
-            }
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("1");
+                }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 2)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("2");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("2");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 3)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("3");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("3");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 4)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("4");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("4");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 5)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("5");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("5");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 6)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("6");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("6");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 7)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("7");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("7");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 8)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("8");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("8");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 9)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("9");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("9");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 10)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("10");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("10");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 11)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("11");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("11");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 12)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("12");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("12");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 13)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("13");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("13");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 14)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("14");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("14");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
+        else if(global.Global.TN == 15)
+        {
+            DatabaseReference reff;
+            reff = FirebaseDatabase.getInstance().getReference("Table").child("15");
+
+            reff.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String Status = dataSnapshot.child("Status").getValue().toString();
+                    ((TextView) findViewById(R.id.tableCurrentStatus)).setText(Status);
+                    ((TextView) findViewById(R.id.tableNumber)).setText("15");
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
+
+
+        }
     }
 
 
